647fb1cb0900028a1b4ddeb7f4c192648067fc74
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-07-30 08:28:01 +0530

    Intial Commit

---
+++ src/test/java/com/jaka/JwtWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithSpringSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
